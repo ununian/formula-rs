01afea1d037e096f44b4bb00c02e279e83b6e1f7
ununian <ununian@outlook.com>
2022-09-24 00:55:22 +0800

    update

--- src/calc/function.rs
+++ src/calc/function.rs
@@ -1,6 +1,9 @@
-use pest::iterators::Pair;
+use std::collections::HashMap;
 
-use crate::calc::calc::Rule;
+use pest::iterators::Pair;
+use serde_json::Value;
+
+use crate::{calc::calc::Rule, ExpValue, Identifier};
 
 #[derive(Debug, Clone)]
 pub struct Function {
@@ -21,6 +24,7 @@ pub enum CompareOperator {
 #[derive(Debug, Clone, PartialEq)]
 pub enum FunctionPart {
     Identifier(String),
+    IdentifierFWithField(String, String),
     CompareExpression(String, CompareOperator, String),
 }
 
@@ -38,10 +42,26 @@ impl FunctionPart {
     }
 
     fn from_single(expression: Pair<Rule>) -> Option<FunctionPart> {
-        if let Rule::function_parameter_item = expression.as_rule() {
+        if Rule::function_parameter_item == expression.as_rule() {
             let inner = expression.into_inner().collect::<Vec<Pair<Rule>>>();
             if inner.len() == 1 {
-                return Some(FunctionPart::Identifier(inner[0].as_str().to_string()));
+                let str = inner[0].as_str().trim();
+                return str
+                    .split(".")
+                    .map(|s| s.to_string())
+                    .collect::<Vec<String>>()
+                    .as_slice()
+                    .split_first()
+                    .map(|(first, rest)| {
+                        if rest.is_empty() {
+                            FunctionPart::Identifier(first.to_string())
+                        } else {
+                            FunctionPart::IdentifierFWithField(
+                                first.to_string(),
+                                rest[0].to_string(),
+                            )
+                        }
+                    });
             } else if inner.len() == 2 {
                 let mut compare_expression = inner[1].clone().into_inner();
 
@@ -89,6 +109,18 @@ impl FunctionPart {
         }
         None
     }
+
+    pub fn get_dependencies(&self) -> Vec<Identifier> {
+        match self {
+            FunctionPart::Identifier(ident) => vec![Identifier {
+                name: ident.clone(),
+            }],
+            FunctionPart::IdentifierFWithField(ident, _) => vec![Identifier {
+                name: ident.clone(),
+            }],
+            _ => vec![],
+        }
+    }
 }
 
 impl Function {
@@ -117,6 +149,54 @@ impl Function {
             None
         } else {
             None
+        }
+    }
+
+    pub fn get_dependencies(&self) -> Vec<Identifier> {
+        self.parts
+            .iter()
+            .flat_map(|p| p.get_dependencies())
+            .collect()
+    }
+
+    pub fn run(&self, json: &Value) -> Option<ExpValue> {
+        match self.name.to_lowercase().as_str() {
+            "sum" => {
+                let data = match self.parts.get(0) {
+                    Some(FunctionPart::Identifier(ident)) => {
+                        json.as_object().unwrap().get(ident)?.as_array().clone()
+                    }
+                    // Some(FunctionPart::IdentifierFWithField(ident, field)) => {
+                    //     for (_, value) in table {
+                    //         if let Some(value) = value.get(ident) {
+                    //             if let Some(value) = value.get(field) {
+                    //                 if let Some(value) = value.as_f64() {
+                    //                     sum += value;
+                    //                 }
+                    //             }
+                    //         }
+                    //     }
+                    // }
+                    _ => return None,
+                };
+
+                println!("{:?}", data);
+
+                Some(ExpValue::Number(1.0))
+            }
+
+            // for part in &self.parts {
+            //     if let FunctionPart::Identifier(ident) = part {
+            //         if let Some(value) = values.iter().find(|v| v.name == *ident) {
+            //             sum += value.value;
+            //         }
+            //     }
+            // }
+            // Some(Identifier {
+            //     name: "sum".to_string(),
+            //     value: sum,
+            // })
+            _ => None,
         }
     }
 }
